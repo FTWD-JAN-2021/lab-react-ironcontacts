0fef7163231a5ab6008182ed0d7defe202e9f2c5
Rastko <rastkodjurica@yahoo.com>
2021-02-09 18:12:17 -0500

    RastkoUpdate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import contacts from './contacts.json';
 
@@ -6,21 +6,32 @@ import contacts from './contacts.json';
 
 console.log(contacts)
 
-function App(){
- const firstFive = contacts.slice(0, 5)
- console.log(firstFive)
- const showFive=()=>{
-   return firstFive.map(eachContact =>{
-     return <li>{eachContact.name} <img src={eachContact.pictureUrl}/></li>
-   })
- }
+function App() {
+  const [stateCelbs, setStateCelbs] = useState(contacts.splice(0, 5))
+  const [otherCelbs, setOtherCelbs] = useState(contacts)
+  const showFive = () => {
+    return stateCelbs.map(eachContact => {
+      return <li>{eachContact.name} <img src={eachContact.pictureUrl} /></li>
+    })
+  }
 
+  const addRandom = () => {
+    console.log('hello')
+    let randomN = Math.floor(Math.random() * otherCelbs.length)
+    let randomCeleb = otherCelbs.splice(randomN, 1)[0]
+    let newCelebs = [...stateCelbs]
 
+    newCelebs.push({ ...randomCeleb })
+
+    console.log(randomCeleb, newCelebs)
+    setStateCelbs(newCelebs)
+  }
 
 
   return (
-    <div>Hello 
-    {showFive()}
+    <div>Hello
+      {showFive()}
+      <button onClick={addRandom}>Add Rendom Actor</button>
     </div>
   )
 }
