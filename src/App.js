50db8789788c1d35859ba46aa04af45c99b6d3a9
Stelian Grosu <stelian.email@yahoo.com>
2021-02-09 17:29:39 -0500

    Stelian adds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App" >
-      </div>
-    );
-  }
+
+
+console.log(contacts)
+
+function App(){
+ const firstFive = contacts.slice(0, 5)
+ console.log(firstFive)
+ const showFive=()=>{
+   return firstFive.map(eachContact =>{
+     return <li>{eachContact.name} <img src={eachContact.pictureUrl}/></li>
+   })
+ }
+
+
+
+
+  return (
+    <div>Hello 
+    {showFive()}
+    </div>
+  )
 }
 
 export default App;
